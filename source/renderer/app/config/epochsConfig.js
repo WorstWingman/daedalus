4ec2a9ecedf1786f18af0ced418bc426fe7bdda0
Nikola Glumac <niglumac@gmail.com>
2019-12-16 15:53:21 +0100

    Use ITN epoch configuration for selfnode

--- source/renderer/app/config/epochsConfig.js
+++ source/renderer/app/config/epochsConfig.js
@@ -13,14 +13,13 @@ export const SLOT_DURATION_TESTNET = 20; // unit: seconds
 export const SLOT_DURATION_DEVELOPMENT = 7; // unit: seconds
 export const SLOT_DURATION_ITN = 2; // unit: seconds
 
-export const SLOTS_TOTAL =
-  isDev || isTest || isIncentivizedTestnetSelfNode ? 150 : 43200;
+export const SLOTS_TOTAL = 43200;
 
 export const EPOCH_LENGTH_BASE_MAINNET = 2160;
 export const EPOCH_LENGTH_BASE_STAGING = 2160;
 export const EPOCH_LENGTH_BASE_TESTNET = 2160;
 export const EPOCH_LENGTH_BASE_DEVELOPMENT = 2;
 
-export const EPOCH_LENGTH_ITN = SLOTS_TOTAL * SLOT_DURATION_ITN; // 1 day / 5 minutes (isDev || isTest || isIncentivizedTestnetSelfNode = true) | unit: seconds
+export const EPOCH_LENGTH_ITN = SLOTS_TOTAL * SLOT_DURATION_ITN; // 1 day | unit: seconds
 
 export const EPOCH_COUNTDOWN_INTERVAL = 1 * 1000; // 1 second | unit: milliseconds;
